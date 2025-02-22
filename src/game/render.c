dadfde5056bb365acc3060377561d5070cb864f4
Mewen Batty <mbatty@2G1.42angouleme.fr>
2025-02-22 12:42:11 +0100

    Started work on sprites

--- src/game/render.c
+++ src/game/render.c
@@ -6,7 +6,7 @@
 /*   By: mbatty <mewen.mewen@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 15:39:37 by mbatty            #+#    #+#             */
-/*   Updated: 2025/02/21 15:49:31 by mbatty           ###   ########.fr       */
+/*   Updated: 2025/02/22 12:40:29 by mbatty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,12 +71,41 @@ void	draw_crosshair(t_ctx *ctx, int x, int y, int size)
 	}
 }
 
+void	draw_sprite(t_ctx *ctx)
+{
+	float	sx = 1.5f * 64.f - ctx->maths.px;
+	float	sy = 1.5f * 64.f - ctx->maths.py;
+	float	sz = 20.f;
+
+	float	CS = cos(ctx->maths.pa);
+	float	SN = sin(ctx->maths.pa);
+
+	float	a = sy * CS - sx * SN;
+	float	b = sx * CS + sy * SN;
+
+	sx = a;
+	sy = b;
+
+	sx = (sx * ((float)ctx->winfo.img->width / 2.f * tan(PI / 180.f * 60.f)) / sy) + ((float)ctx->winfo.img->width / 2.f);
+	sy = (sz * (float)ctx->winfo.img->height / sy) + ((float)ctx->winfo.img->height / 2.f);
+
+	// t_rsquare	sqvars;
+
+	// sqvars = init_rsquare_vars(sy, sx - ((32 / 2) / (distance(1.5f * 64.f, 1.5f * 64.f, ctx->maths.px, ctx->maths.py) / 32)), );
+	for (int x = 0; x < (MAP_S * 32) / distance(1.5f * 64.f, 1.5f * 64.f, ctx->maths.px, ctx->maths.py); x++)
+	{
+		for (int y = 0; y < (MAP_S * 32) / distance(1.5f * 64.f, 1.5f * 64.f, ctx->maths.px, ctx->maths.py); y++)
+			safe_put_pixel(ctx->winfo.img, sx + x, sy + y, 0x00FFFFFF);
+	}
+}
+
 void	render_frame(t_ctx *ctx)
 {
 	gettimeofday(&ctx->ginfo.start_time, NULL);
 	draw_sky(ctx);
 	draw_cubes(ctx);
 	draw_minimap(ctx);
+	draw_sprite(ctx);
 	draw_crosshair(ctx, ctx->winfo.img->width / 2 - 2,
 		ctx->winfo.img->height / 2 - 2, 4);
 	draw_fps(ctx);
